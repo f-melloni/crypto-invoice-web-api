346d568beccc3b9a5389c6d7214e16e1cea1cf04
Samuel Lachman <samlachman@centrum.cz>
2018-09-03 10:28:57 +0200

    Mail url and attachment

--- WebApi/Services/EmailManager.cs
+++ WebApi/Services/EmailManager.cs
@@ -15,6 +15,8 @@ namespace WebApi.Services
 
         public static void SendMailToPaymentReciever(Invoice invoice, TransactionConfirmedModel confirmation)
         {
+            string url = $"https://live.blockcypher.com/{confirmation.CurrencyCode.ToLower()}/tx/{confirmation.TXID}";
+
             string subject = $"CryptoMail - incoming payment from {invoice.Recipient} confirmed";
             string body = System.IO.File.ReadAllText("wwwroot/web-api-static/templates/email/paymentConfirmationReciever.html");
             body = body.Replace("{Title}", subject)
@@ -25,15 +27,19 @@ namespace WebApi.Services
                         .Replace("{AmountCrypto}", confirmation.Amount.ToString())
                         .Replace("{CurrencyCrypto}", confirmation.CurrencyCode)
                         .Replace("{TargetAddressCrypto}", confirmation.Address)
-                        .Replace("{TransactionTimestamp}", invoice.DateReceived.ToString());
+                        .Replace("{TransactionTimestamp}", invoice.DateReceived.ToString())
+                        .Replace("{BlockchainUrl}", $"<a href=\"{url}\">{url}</a>");
+            string attachment = !string.IsNullOrEmpty(invoice.File) ? $"{invoice.File}|{invoice.FileName}|{invoice.FileMime}" : "";
 
             EmailSender sender = new EmailSender(_configuration);
-            Email email = sender.CreateEmailEntity("info@octupus.com", invoice.CreatedBy.Email, body, subject, null);
+            Email email = sender.CreateEmailEntity("info@octupus.com", invoice.CreatedBy.Email, body, subject, attachment);
             sender.AddEmailToQueue(email);
         }
 
         public static void SendMailToPaymentSender(Invoice invoice, TransactionConfirmedModel confirmation)
         {
+            string url = $"https://live.blockcypher.com/{confirmation.CurrencyCode.ToLower()}/tx/{confirmation.TXID}";
+
             string subject = $"CryptoMail - outcoming payment to {invoice.CreatedBy.Email} confirmed";
             string body = System.IO.File.ReadAllText("wwwroot/web-api-static/templates/email/paymentConfirmationSender.html");
             body = body.Replace("{Title}", subject)
@@ -44,10 +50,12 @@ namespace WebApi.Services
                         .Replace("{AmountCrypto}", confirmation.Amount.ToString())
                         .Replace("{CurrencyCrypto}", confirmation.CurrencyCode)
                         .Replace("{TargetAddressCrypto}", confirmation.Address)
-                        .Replace("{TransactionTimestamp}", invoice.DateReceived.ToString());
+                        .Replace("{TransactionTimestamp}", invoice.DateReceived.ToString())
+                        .Replace("{BlockchainUrl}", $"<a href=\"{url}\">{url}</a>");
+            string attachment = !string.IsNullOrEmpty(invoice.File) ? $"{invoice.File}|{invoice.FileName}|{invoice.FileMime}" : "";
 
             EmailSender sender = new EmailSender(_configuration);
-            Email email = sender.CreateEmailEntity("info@octupus.com", invoice.Recipient, body, subject, null);
+            Email email = sender.CreateEmailEntity("info@octupus.com", invoice.Recipient, body, subject, attachment);
             sender.AddEmailToQueue(email);
         }
     }
